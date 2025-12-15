8f44cda8b9aa4e742d783f518b3be19e5a658cde
Banu <banuraj2402@gmail.com>
2025-12-15 09:48:45 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function addBookToList(title, author, isbn) {
+
+    const newRowHTML = 
+        <tr>
+            <td>title</td>
+            <td>author</td>
+            <td>isbn</td>
+            <td><button class="delete">X</button></td>
+        </tr>
+    ;
+
+    bookList.innerHTML += newRowHTML;
+}
