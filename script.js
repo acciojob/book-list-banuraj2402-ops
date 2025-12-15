49ed403623c4046379ab88ab641a7cbfe73bbf51
Banu <banuraj2402@gmail.com>
2025-12-15 09:55:53 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 //your JS code here. If required.
-function addBookToList(title, author, isbn) {
+const bookForm = document.getElementById('book-form'); 
+const bookList = document.getElementById('book-list'); 
 
-    const newRowHTML = 
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+
+ 
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value;
+
+    if (title === '' || author === '' || isbn === '') {
+        alert('Please fill in all fields.');
+        return;
+    }
+
+    const newRowHTML = `
         <tr>
-            <td>title</td>
-            <td>author</td>
-            <td>isbn</td>
-            <td><button class="delete">X</button></td>
+            <td>${title}</td>
+            <td>${author}</td>
+            <td>${isbn}</td>
+            <td><button class="delete">X</button></td> 
         </tr>
-    ;
+    `;
+    bookList.innerHTML += newRowHTML; 
 
-    bookList.innerHTML += newRowHTML;
-}
+
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+});
+
+
+
+bookList.addEventListener('click', (e) => {
+  
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.parentElement.remove();
+    }
+});
